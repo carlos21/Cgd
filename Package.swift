5e571b6627f9afd54bf5a963a230c6957035ad43
Carlos Duclós <darkzeratul64@gmail.com>
2018-07-30 05:27:50 +0000

    fix

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
 // swift-tools-version:3.1
-
 import PackageDescription
 
 let package = Package(
     name: "Cgd",
     pkgConfig: "gdlib",
     providers: [
-        .brew(["gd"]),
-        .apt(["libgd-dev"])
+        .Brew("gd"),
+        .Apt("libgd-dev")
     ]
 )
